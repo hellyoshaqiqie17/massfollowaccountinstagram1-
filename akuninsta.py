a22f694d844e711fc092dcf101f3fae38262e88a
Hellyoshaqiqie <77888513+hellyoshaqiqie17@users.noreply.github.com>
2023-05-17 18:11:41 +0700

    Update akuninsta.py

--- akuninsta.py
+++ akuninsta.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import proxy_service
 
-# Akun yang akan difolloww
+# Akun yang akann difollow
 akun_tujuan = "https://www.instagram.com/hellyoshaqiqieeee/"
 
 # Membuka browser
